55dd69728e3bfce32be06081f2fad7e422033ef8
Andrew <browner12@gmail.com>
2016-04-11 12:01:30 -0500

    add route middleware

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,8 @@
 <?php
 
-Route::get('pages/manage', ['as' => 'pages.manage', 'uses' => '\claritytech\cms\controllers\PageController@manage']);
-Route::resource('pages', '\claritytech\cms\controllers\PageController');
+Route::group(['middleware' => ['web']], function(){
+
+    Route::get('pages/manage', ['as' => 'pages.manage', 'uses' => '\claritytech\cms\controllers\PageController@manage']);
+    Route::resource('pages', '\claritytech\cms\controllers\PageController');
+
+});
